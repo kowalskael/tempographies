2fc5cb460a7755d26e62fc9ffb7ae2ca60b72504
kowalska.el <kowalska.el@gmail.com>
2024-08-12 15:47:13 +0200

    console.log for Jakub

--- src/functions.js
+++ src/functions.js
@@ -60,11 +60,8 @@ class NodeObject {
     click() {
       if ((mouseX > this.x) && (mouseX < this.x + this.width) &&
       (mouseY > this.y) && (mouseY < this.y + this.height)) {
-        console.log('clicked')
         this.clicked = 'true';
         this.clickable = 'false';
-
-        
       }
     }
 
--- src/index.js
+++ src/index.js
@@ -29,21 +29,14 @@ function preload() {
 
     // funkcja, która transformuje słowa z nodeArray w description na linki
     for (let i = 0; i < nodeData.length; i++) {
-       
         for(let j = 0; j < nodeData[i].networkArray.length; j++) {
-
             for(let k = 0; k < nodeData[i].networkArray[j].length; k++) {
-    
                 if(nodeData[i].description.indexOf(nodeData[i].networkArray[j][k]) > -1) {
-                    
                     let newString = nodeData[i].description.replace(nodeData[i].networkArray[j][k], '<a style="color:blue; margin: 0px; padding: 0px; " id="' + nodeData[i].networkArray[j][k] + '">' + nodeData[i].networkArray[j][k] + '</a>')
                     nodeData[i].description = newString;
-          
                 }
             }
-           
         }  
-        
     }
 
     let nodeW = 220;
@@ -61,95 +54,77 @@ function setup() {
     let canvas = createCanvas(grpSpaceW, innerHeight - 40);
     canvas.parent('#graph');
 
-    
     function initialClick() {
         if(stage === 'init') {
             grpSpace.addEventListener("click", function() {
                 for(let i = 0; i < nodes.length; i++) {
                 
-                if ((mouseX > nodes[i].x) && (mouseX < nodes[i].x + nodes[i].width) &&
-                (mouseY > nodes[i].y) && (mouseY < nodes[i].y + nodes[i].height)) {
+                    if ((mouseX > nodes[i].x) && (mouseX < nodes[i].x + nodes[i].width) &&
+                    (mouseY > nodes[i].y) && (mouseY < nodes[i].y + nodes[i].height)) {
 
-                if(flag === 'true') {
-           
-                nodes[i].click();
-                flag = 'false';
+                        if(flag === 'true') {
             
-        nodes = nodes.filter(function(el) { return el.clicked === "true"; })
-                stage = 'graph';
-                console.log(nodes.length)
-
-                for(let i = 0; i < nodes.length; i++) {
-                    descDiv = document.createElement('div');
-                    descDiv.setAttribute("id", "descDiv");
-                    document.getElementById('graph').appendChild(descDiv);
-                    descText = document.createElement("id");
-                    descText.innerHTML = nodes[0].description;
-                    document.getElementById('descDiv').appendChild(descText);
-
+                            nodes[i].click();
+                            flag = 'false';
                 
-                                        
+                            nodes = nodes.filter(function(el) { return el.clicked === "true"; })
+                            stage = 'graph';
 
-                    if(nodes[0].x < grpSpaceW/3 && nodes[0].y < grpSpaceH/2) {
-                        
-                    }
-    
-                    if(nodes[0].x > grpSpaceW/3 && nodes[0].x < grpSpaceW/3*2 && nodes[0].y < grpSpaceH/2) {
-                        
-                    }
-    
-                    if(nodes[0].x > grpSpaceW/3*2 && nodes[0].y < grpSpaceH/2) {
-                        
-                    }
-    
-                    if(nodes[0].x < grpSpaceW/3 && nodes[0].y > grpSpaceH/2) {
-                        descDiv.style.left = nodes[0].x + nodes[0].width + 50 + "px";
-                    }
-    
-                    if(nodes[0].x > grpSpaceW/3 && nodes[0].x < grpSpaceW/3*2 && nodes[0].y > grpSpaceH/2) {
-                        descDiv.style.left = 200 + "px";
-                    }
-    
-                    if(nodes[0].x > grpSpaceW/3*2 && nodes[0].y > grpSpaceH/2) {
-    
-                    }
+                            for(let i = 0; i < nodes.length; i++) {
+                                descDiv = document.createElement('div');
+                                descDiv.setAttribute("id", "descDiv");
+                                document.getElementById('graph').appendChild(descDiv);
+                                descText = document.createElement("id");
+                                descText.innerHTML = nodes[0].description;
+                                document.getElementById('descDiv').appendChild(descText);                   
 
-                    console.log(nodes[0].nodeArray)
-
-                    for(let j = 0; j < nodes[0].nodeArray.length; j++) {
-                
-                        for(let k = 0; k < nodes[0].nodeArray[j].length; k++) {
-                                                                    
-                                let newLink = document.getElementById(nodes[0].nodeArray[j][k])
-                                console.log(newLink)
-                                                                
-                                newLink.addEventListener("click", function ()
-                                    {
-                                console.log('clicked')
-                                //create new ellipse with a data
-                                //add data to nodes
-                                //hide the div with text (and clear data ? )
-                            })     
+                                if(nodes[0].x < grpSpaceW/3 && nodes[0].y < grpSpaceH/2) {
+                        
+                                }
+        
+                                if(nodes[0].x > grpSpaceW/3 && nodes[0].x < grpSpaceW/3*2 && nodes[0].y < grpSpaceH/2) {
                             
+                                }
+        
+                                if(nodes[0].x > grpSpaceW/3*2 && nodes[0].y < grpSpaceH/2) {
+                            
+                                }
+        
+                                if(nodes[0].x < grpSpaceW/3 && nodes[0].y > grpSpaceH/2) {
+                                    descDiv.style.left = nodes[0].x + nodes[0].width + 50 + "px";
+                                }
+        
+                                if(nodes[0].x > grpSpaceW/3 && nodes[0].x < grpSpaceW/3*2 && nodes[0].y > grpSpaceH/2) {
+                                    descDiv.style.left = 200 + "px";
+                                }
+
+                                if(nodes[0].x > grpSpaceW/3*2 && nodes[0].y > grpSpaceH/2) {
+        
+                                }
+
+                                console.log(nodes[0].name)
+                                console.log(nodes[0].description)
+
+                                for(let j = 0; j < nodes[0].nodeArray.length; j++) {
+                                    for(let k = 0; k < nodes[0].nodeArray[j].length; k++) {
+                                                                        
+                                        let newLink = document.getElementById(nodes[0].nodeArray[j][k])
+                                                                        
+                                        newLink.addEventListener("click", function () {
+                                            console.log('clicked')
+                                        //create new ellipse with a data
+                                        //add data to nodes
+                                        //hide the div with text (and clear data ? )
+                                        })     
+                                
+                                    } 
+                                }          
+                            }    
                         }
-                       
-                    }  
-                        
-                        
                     }
-
-                
-                
-            }
-        }
-      }
-                
-
-                // można zrobić tak, że ten jeden aktywny el. jest wpychany w inną zmienną niż nodes, i potem tylko to się renderuje?
-             
-            })
-
-            
+                }
+                // można zrobić tak, że ten jeden aktywny el. jest wpychany w inną zmienną niż nodes, i potem tylko to się rende
+            })  
         }
 
         if(stage != 'init') {
@@ -159,7 +134,6 @@ function setup() {
     }
     
     initialClick();
-
 }
 
 function draw() {
