20e8f4468bf10b2d437196ad9038be0519c848ec
kowalska.el <kowalska.el@gmail.com>
2024-08-17 19:12:58 +0200

    small changes

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ let grpSpaceW = grpSpace.offsetWidth;
 const nodeW = 220;
 const nodeH = 135;
 
-let nxtNodeX = 0, nxtNodeY = 0, nxtNodeDist = 0;
-let updatedX = 0, updatedY = 0;
+let prevNodeX = 0, prevNodeY = 0, nodeDist = 0;
+let updateNodeX = 0, updateNodeY = 0;
 
 let indexPrnt = document.getElementById('index-input');
 
@@ -101,7 +101,6 @@ function init(node) {
         return el.clicked === true;
     })
 
-    console.log(nodes)
     node.x = 70;
     node.y = 70;
 
@@ -109,7 +108,7 @@ function init(node) {
     node.clickable = true;
 
     // add description to index-input
-    indexPrnt.innerHTML += '<p>' + node.indexInput + '</p>';
+    indexPrnt.innerHTML += `<p>${node.indexInput}</p>`;
 
     stageInit = false;
 }
@@ -135,8 +134,8 @@ function addNode(node) {
 
     descDiv.style.display = 'block';
     descText.innerHTML = node.description;
-    descDiv.style.left = 350 + "px";
-    descDiv.style.top = 50 + "px";
+    descDiv.style.left = node.x + "px";
+    descDiv.style.top = node.y + "px";
     console.log('clicked initial ' + node.name);
 
     for (let j = 0; j < node.networkArray.length; j++) {
@@ -155,22 +154,31 @@ function createNewNode(node) {
                 continue;
             }
 
-            nxtNodeX = nodes[nodes.length - 1].x;
-            nxtNodeY = nodes[nodes.length - 1].y;
+            //pozycja poprzedniego node'a
+            prevNodeX = nodes[nodes.length - 1].x;
+            prevNodeY = nodes[nodes.length - 1].y;
 
-            nxtNodeDist = Math.hypot(nodeW, nodeH);
+            console.log(prevNodeX, prevNodeY);
 
-            updatedX = Math.cos(toRadians(360)) * nxtNodeDist + nxtNodeX;
-            updatedY = Math.sin(toRadians(45)) * nxtNodeDist + nxtNodeY;
+            // dystans na podstawie poprzedniej pozycji
+            nodeDist = Math.hypot(nodeW, nodeH);
 
-            newNode = new NodeObject(nodeData[l], updatedX, updatedY);
+            console.log(nodeDist);
+
+            // update X i Y o dystans
+            updateNodeX = Math.cos(toRadians(360)) * nodeDist + prevNodeX;
+            updateNodeY = Math.sin(toRadians(45)) * nodeDist + prevNodeY;
+
+            console.log(updateNodeX, updateNodeY)
+
+            newNode = new NodeObject(nodeData[l], updateNodeX, updateNodeY);
             nodes.push(newNode);
             descDiv.style.display = 'none';
             // add description to index-input
-            indexPrnt.innerHTML += '<p>' + nodeData[l].indexInput + '</p>';
+            indexPrnt.innerHTML += `<p>${nodeData[l].indexInput}</p>`;
 
-            nxtNodeX = updatedX;
-            nxtNodeY = updatedY;
+            // po dodaniu obiektu przesuń #graph do góry i w lewo
+            // graph.style.transform = `translate(${ }px, ${ }px)`;
 
             console.log(nodes);
         }
@@ -184,6 +192,12 @@ function draw() {
     for (let i = 0; i < nodes.length; i++) {
         nodes[i].render();
     }
+
+    //stroke(126);
+    //line(100, 300, 400, 500);
+    //draw a line between objects center, x1, y1 = pozycja jednego okręgu, x2, y2 = pozycja drugiego okręgu
+    //można zapisać jako tablę z obiektami, która jest uzupełniana podczas tworzenia kolejnego obiektu
+    //render będzie się odbywać po przesłaniu danych do tabeli
 
     if (!stageInit) {
         for (let i = 0; i < nodes.length; i++) {
